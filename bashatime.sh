e847f276a4f61c9e3889ff287c30dac4ef0d2fa3
Emma <hello@prpl.wtf>
2025-10-07 22:36:38 +0200

    feat: add support for unstaged files, fancy logo, fancier logs and add .bashatimerc

--- bashatime.sh
+++ bashatime.sh
@@ -1,28 +1,48 @@
 #!/bin/bash
 
+# shellcheck disable=SC1091
+source .bashatimerc 2>/dev/null
+
 get_hash() {
-    git ls-files -z | xargs -0 md5sum | sort | md5sum
+    git ls-files --others --exclude-standard --cached -z | xargs -0 md5sum | sort | md5sum
 }
 
+printout() {
+    case "$1" in
+    verbose)
+        if [[ $LOG_VERBOSE == 1 ]]; then
+            echo -e "\e[30;42;1m bashatime.sh \e[0;30;47m $(date +"%H:%M:%S") \e[0m $2"
+        fi
+        ;;
+    esac
+}
+
+echo -e ""
+echo -e "" \
+    "\e[32;1m▟▉▙▝▙▝▙ \e[0;30;42;1m bashatime.sh \e[0m \n" \
+    "\e[32;1m▜▉▛▗▛▗▛ \e[0;2m© 2025 Emma (prpl.wtf)\e[0m \n"
+
+sleep 2
+
 last_hash=$(get_hash)
-echo "initial hash: $last_hash"
+printout verbose "initial hash: $last_hash"
 
 should_heartbeat=false
 heartbeat_timer=0
-echo "initialized: should_heartbeat=$should_heartbeat, heartbeat_timer=$heartbeat_timer"
+printout verbose "initialized: should_heartbeat=$should_heartbeat, heartbeat_timer=$heartbeat_timer"
 
 while true; do
-    echo "waiting for changes..."
+    printout verbose "waiting for changes..."
     output="$(inotifywait -q -t 1 -r -e modify,create ./)"
     # shellcheck disable=SC2181
     if [[ $? == "0" ]]; then
-        echo "inotifywait triggered: $output"
+        printout verbose "inotifywait triggered: $output"
         current_time=$(date +%s)
-        echo "current time: $current_time"
+        printout verbose "current time: $current_time"
 
         if [[ $should_heartbeat == "false" ]]; then
             heartbeat_timer=$((current_time + 30))
-            echo "setting heartbeat timer to: $heartbeat_timer"
+            printout verbose "setting heartbeat timer to: $heartbeat_timer"
         fi
 
         if [[ "$output" =~ ^(.*/)[[:space:]]([A-Z]+)[[:space:]](.*)$ ]]; then
@@ -30,42 +50,42 @@ while true; do
             action="${BASH_REMATCH[2]}"
             filename="${BASH_REMATCH[3]}"
             filepath="${dir}${filename}"
-            echo "parsed: dir=$dir, action=$action, filename=$filename, filepath=$filepath"
+            printout verbose "parsed: dir=$dir, action=$action, filename=$filename, filepath=$filepath"
 
             # only track if file is actually in git
-            if git ls-files --error-unmatch "$filepath" &>/dev/null; then
-                echo "file is tracked by git"
+            if git ls-files --others --exclude-standard --cached --error-unmatch "$filepath" &>/dev/null; then
+                printout verbose "file is tracked by git"
                 should_heartbeat=true
-                echo "should_heartbeat set to true"
+                printout verbose "should_heartbeat set to true"
             else
-                echo "file not tracked by git, ignoring"
+                printout verbose "file not tracked by git, ignoring"
             fi
         else
-            echo "regex didn't match output"
+            printout verbose "regex didn't match output"
         fi
     fi
 
     current_time=$(date +%s)
-    echo "checking heartbeat: should_heartbeat=$should_heartbeat, current_time=$current_time, heartbeat_timer=$heartbeat_timer"
+    printout verbose "checking heartbeat: should_heartbeat=$should_heartbeat, current_time=$current_time, heartbeat_timer=$heartbeat_timer"
 
     if [[ ($should_heartbeat == true) && ($current_time -ge $heartbeat_timer) ]]; then
-        echo "heartbeat conditions met, checking hash..."
+        printout verbose "heartbeat conditions met, checking hash..."
         current_hash=$(get_hash)
-        echo "current hash: $current_hash"
+        printout verbose "current hash: $current_hash"
 
         if [ "$last_hash" != "$current_hash" ]; then
-            echo "hash changed, sending to wakatime: $filepath"
+            printout verbose "hash changed, sending to wakatime: $filepath"
             wakatime-cli \
                 --time "$current_time" \
                 --write true \
                 --entity "$filepath" \
                 --plugin "prplwtf/bashatime.sh"
             last_hash=$current_hash
-            echo "wakatime sent, updated last_hash"
+            printout verbose "wakatime sent, updated last_hash"
         else
-            echo "hash unchanged, skipping wakatime"
+            printout verbose "hash unchanged, skipping wakatime"
         fi
         should_heartbeat=false
-        echo "reset should_heartbeat to false"
+        printout verbose "reset should_heartbeat to false"
     fi
 done
